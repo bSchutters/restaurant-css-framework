f76d1193aaa62d288ab4d222f3166f24cf3c81f9
bSchutters <bryan021@hotmail.be>
2023-10-10 21:14:12 +0200

    cookies desktop + littles fixes

--- src/main.js
+++ src/main.js
@@ -2,22 +2,35 @@ import "/css/main.css";
 import "/css/reset.css";
 import "/css/tailwind.css";
 
+const btnCookiesDesk = document.getElementById("btn-cookies-desk");
+const popCookiesDesk = document.getElementById("pop-cookies-desk");
+
+if (btnCookiesDesk) {
+  btnCookiesDesk.onclick = function () {
+    popCookiesDesk.classList.add("hide");
+  };
+}
+
 // BURGER MENU
 const iconBurger = document.getElementById("icon-burger");
 const iconClose = document.getElementById("icon-close");
 const navBurger = document.getElementById("nav-burger");
 const body = document.getElementsByTagName("body")[0];
 
-iconBurger.onclick = function () {
-  window.scrollTo(0, 0);
-  body.classList.add("overflow-hidden");
-  navBurger.classList.remove("hidden");
-};
+if (iconBurger) {
+  iconBurger.onclick = function () {
+    window.scrollTo(0, 0);
+    body.classList.add("overflow-hidden");
+    navBurger.classList.remove("hidden");
+  };
+}
 
-iconClose.onclick = function () {
-  body.classList.remove("overflow-hidden");
-  navBurger.classList.add("hidden");
-};
+if (iconClose) {
+  iconClose.onclick = function () {
+    body.classList.remove("overflow-hidden");
+    navBurger.classList.add("hidden");
+  };
+}
 
 //Collapse
 
@@ -25,28 +38,35 @@ const titreSignature = document.getElementById("title-signature");
 const contentSignature = document.getElementById("menu-degustation-content");
 const footer = document.getElementsByTagName("footer")[0];
 
-titreSignature.onclick = function () {
-  contentSignature.classList.toggle("hidden");
-  footer.classList.toggle("fixed");
-};
-
+if (titreSignature) {
+  titreSignature.onclick = function () {
+    contentSignature.classList.toggle("hidden");
+    footer.classList.toggle("fixed");
+  };
+}
 const titreStarters = document.getElementById("title-lacarte-starters");
 const contentStarters = document.getElementById("content-lacarte-starters");
 
-titreStarters.onclick = function () {
-  contentStarters.classList.toggle("hidden");
-};
+if (titreStarters) {
+  titreStarters.onclick = function () {
+    contentStarters.classList.toggle("hidden");
+  };
+}
 
 const titreMain = document.getElementById("title-lacarte-main-courses");
 const contentMain = document.getElementById("content-lacarte-main-courses");
 
-titreMain.onclick = function () {
-  contentMain.classList.toggle("hidden");
-};
+if (titreMain) {
+  titreMain.onclick = function () {
+    contentMain.classList.toggle("hidden");
+  };
+}
 
 const titreDesserts = document.getElementById("title-lacarte-desserts");
 const contentDesserts = document.getElementById("content-lacarte-desserts");
 
-titreDesserts.onclick = function () {
-  contentDesserts.classList.toggle("hidden");
-};
+if (titreDesserts) {
+  titreDesserts.onclick = function () {
+    contentDesserts.classList.toggle("hidden");
+  };
+}
